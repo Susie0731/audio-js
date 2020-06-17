2986473cc88693064eac8b2f2314f23663d63158
unknown <gongyusi@game.ntes>
2020-06-17 19:56:05 +0800

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.myapplication
+
+import android.app.Activity
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebResourceError
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_main.*
+import android.webkit.WebResourceRequest as WebResourceRequest1
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val myWebUrl: String = "file:///android_asset/test.html"
+       webView.loadUrl(myWebUrl)
+    }
+    class MyWebViewClient internal constructor(private val activity: Activity) : WebViewClient() {
+
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest1?): Boolean {
+            val url: String = request?.url.toString();
+            view?.loadUrl(url)
+            return true
+        }
+
+        override fun shouldOverrideUrlLoading(webView: WebView, url: String): Boolean {
+            webView.loadUrl(url)
+            return true
+        }
+
+        override fun onReceivedError(view: WebView, request: WebResourceRequest1, error: WebResourceError) {
+            Toast.makeText(activity, "Got Error! $error", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
